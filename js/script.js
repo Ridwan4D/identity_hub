9979d02b93e00746cbe406cdf8bf341040242e14
Ridwan4D <ridwanul161@gmail.com>
2025-02-22 15:56:52 +0600

    testimonial section created

---
+++ js/script.js
@@ -0,0 +1,20 @@
+new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 20,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.go_next',
+        prevEl: '.go_prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
