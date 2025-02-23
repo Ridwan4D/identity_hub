0b3893d4fd00dadfb352642a3f0f4862b0a02d5a
Ridwan4D <ridwanul161@gmail.com>
2025-02-23 13:35:50 +0600

    service card bg effect update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+const serviceCards = document.querySelectorAll('.service_card');
+const serviceCardBlurs = document.querySelectorAll('.service_card_blur');
+
+serviceCards.forEach((card, index) => {
+    card.addEventListener('mouseenter', () => {
+        if (serviceCardBlurs[index]) {
+            serviceCardBlurs[index].style.filter = "blur(39px)";
+            serviceCardBlurs[index].style.height = "100px";
+            serviceCardBlurs[index].style.width = "200px)";
+        }
+    });
+    
+    card.addEventListener('mouseleave', () => {
+        if (serviceCardBlurs[index]) {
+            serviceCardBlurs[index].style.filter = "blur(20px)";
+            serviceCardBlurs[index].style.height = "50px";
+            serviceCardBlurs[index].style.width = "100px)";
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 new Swiper('.swiper', {
     loop: true,
     // spaceBetween: 20,
